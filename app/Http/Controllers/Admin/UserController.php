65c6a6165062cb1e63c663788821886b0ed54998
abbas0506 <abbas.sscs@gmail.com>
2024-10-31 11:14:03 +0500

    31102401

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
     public function index()
     {
         //
-        $users = User::where('id', '<=', 3)->get();
+        $users = User::where('id')->get();
         return view('admin.users.index', compact('users'));
     }
 
