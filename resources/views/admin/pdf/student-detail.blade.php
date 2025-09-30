273b7cf392fe5e7902faf199d2e784c5855df881
abbas0506 <abbas.sscs@gmail.com>
2025-09-30 09:08:38 +0500

    XI Class Demo

--- resources/views/admin/pdf/student-detail.blade.php
+++ resources/views/admin/pdf/student-detail.blade.php
@@ -68,7 +68,7 @@ $roman = config('global.romans');
                     <tbody>
                         <tr class="text-xs">
                             <td class="text-left">Total Students: {{ $section->students->count() }}</td>
-                            <td class="text-right">Printed on {{ now()->format('d-M-Y')}}</td>
+                            <td class="text-right">Printed on {{ now()->format('d-M-Y')}} it has been changed</td>
                         </tr>
                     </tbody>
                 </table>
